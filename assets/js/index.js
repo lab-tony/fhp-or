de5cc98241fcf76961bd61f0f0dc406b1774ffc8
Antonio Galindo <tonse@protonmail.com>
2026-05-25 18:36:12 +0200

    [FHP-OR] Add and configure bundler Vite.js

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-import { MobileNav } from './MobileNav.js';
-
-/**
- * Initializes the mobile navigation by creating an instance of the class MobileNav and passing the relevant parameters
- */
-new MobileNav();
---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import { MobileNav } from './MobileNav.js';
+
+/* Import CSS files */
+import '../css/reset.css';
+import '../css/variables.css';
+import '../css/typography.css';
+import '../css/layout.css';
+import '../css/grid.css';
+import '../css/theme.css';
+import '../css/style.css';
+import '../css/card.css';
+import '../css/nav.css';
+import '../css/news.css';
+
+/**
+ * Initializes the mobile navigation by creating an instance of the class MobileNav and passing the relevant parameters
+ */
+new MobileNav();
