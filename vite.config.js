96edf6d549b01b6ce86197d1036aea13074ce7b3
Antonio Galindo <tonse@protonmail.com>
2026-05-25 18:44:27 +0200

    [FHP-OR] Configure multi page app in Vite.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   build: {
     outDir: 'build',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        project: 'projekt.html',
+        impressum: 'impressum.html',
+      },
+    },
   },
 });
