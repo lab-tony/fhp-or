e84b2db1f84d765783eba53bcb9719c30a6fd459
Antonio Galindo <tonse@protonmail.com>
2026-05-26 18:55:09 +0200

    [FHP-OR] Add React and configure dev tools

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,36 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
 export default [
   {
     ignores: ['build/**'],
   },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   prettier,
 ];
